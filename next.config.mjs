223b5c18a17c5a5779a212355fbf1424a5b50bcd
Harlem Hernandez <harlemj-hernandezm@unilibre.edu.co>
2025-11-27 17:18:18 -0500

    Frontend Terminado falta utilizar endpoints correspondiente

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'standalone', // Optimizes for Docker deployments
+}
+
+export default nextConfig
